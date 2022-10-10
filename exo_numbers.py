e5ff4fe2c7b4350a3fe792bdb83d74c570ca0c38
mlamamra <mlamamra@dawan.fr>
2022-10-10 15:30:38 +0200

    exo numbers

---
+++ exo_numbers.py
@@ -0,0 +1,8 @@
+# %%
+# manipulations sur les nombres
+# 1. saisir trois fois trois entiers
+# 2. calculer la moyenne arithmétique
+# 3. calculer l'écart type
+# std = sqrt(1/n sum((x - moy)**2))
+# utiliser la fonction round()
+# pour afficher le résultat avec 2 chiffres significatifs
