976666be2ce7a78c9135b7e8e7fe655d04704b8d
mlamamra <mlamamra@dawan.fr>
2022-10-11 10:04:43 +0200

    exo sequence triangle

--- exo_sequences.py
+++ exo_sequences.py
@@ -21,3 +21,11 @@ else:
 #   ^
 #  ^^^
 # ^^^^^
+
+h, motif = 3, "^"
+for i in range(1, h + 1):
+    # pour chaque ligne
+    # combien d'espaces et combien de motifs 
+    print((h - i) * " " + (2 * i - 1) * motif)
+# %%
+# %%
