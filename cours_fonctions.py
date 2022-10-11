b1080a2dad06dfd52bd37ded23fbcc793f5c108b
mlamamra <mlamamra@dawan.fr>
2022-10-11 17:03:07 +0200

    exo template: fonction correction

--- cours_fonctions.py
+++ cours_fonctions.py
@@ -1,6 +1,7 @@
 # %%
 # définition et appel
 # définition
+from ast import keyword
 from sys import ps2
 
 
@@ -76,5 +77,87 @@ func(1)
 # SyntaxError
 # func(p2=1, 2)
 
+
+# %%
+# paramètres positionnels supplémentaires
+def func(p1, p2, *params):
+    """
+    expliquer comment utiliser *params
+    """
+    return p1, p2, params
+
+func(1, 2, 3, 4, 5)
+# non obligatoires
+func(1, 2)
+
+# %%
+def func(p1, p2="dflt", **kwargs):
+    """
+    expliquer comment utiliser **kwargs
+    """
+    return p1, p2, kwargs
+
+func(1, option1=3, option2=4)
+# %%
+# *values dans la signature de print
+# print()
+
+# %%
+# * à l'appel
+def func(p1, p2, *params):
+    return p1, p2, params
+params = (1, 2, 3, 4, 5)
+# désagréable
+# func(params[0], params[1], params[2], params[3], params[4])
+# agréable
+func(*params)
+# %%
+def func(p1, p2, p3):
+    return p1, p2, p3
+
+keywords = {"p2": 2, "p1": 1, "p3": 3}
+# func(keywords["p1"], keywords["p2"], keywords["p3"])
+func(**keywords)
+
+# %%
+# portée des variables: variables globales
+def do_thing():
+    print(f"x dans la fonction: {x, id(x)}")
+
+x = 10
+
+do_thing()
+print(f"x hors de la fonction: {x, id(x)}")
+# x est globale à la fonction do_thing
+
+
+# %%
+# portée des variables: variables locales à une fonction
+def do_thing():
+    x = 5
+    print(f"x local  à la fonction: {x, id(x)}")
+
+x = 10
+
+do_thing()
+print(f"x global hors de la fonction: {x, id(x)}")
+# le x global n'est pas mis à jour dans la fonction
+# un x local est créé dans la portée de la fonction,
+# et n'existe que le temps d'exécution de la fonction
+
+# %%
+# portée des variables: variables locales à une fonction
+def do_thing():
+    global x, y
+    x = 5
+    print(f"x global dans la fonction: {x, id(x)}")
+
+x, y = 10, 15
+
+do_thing()
+print(f"x global hors de la fonction: {x, id(x)}")
+# le x global est utilisé dans la fonciton en tant que tel
+# le x global est fonc modifié par la fonction
+#la valeur modifiée est disponible après la fonction
 
 # %%
