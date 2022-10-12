a9ac1313700c29826c39c18b10bc5061c44f76af
mlamamra <mlamamra@dawan.fr>
2022-10-12 10:02:48 +0200

    exo fonctions

--- misc.py
+++ misc.py
@@ -3,6 +3,37 @@
 # (chaine de mots séparés par " ")
 # qui ne conserve que les mots d'au moins 4 caractères
 
+# version impérative
+def strip_lil_words(text: str, min_length: int):
+    cleaned = []
+    for word in text.split():
+        if len(word) >= min_length:
+            cleaned.append(word)
+    return " ".join(cleaned)
+
+# version "sucre syntaxique" 
+def strip_lil_words(text: str, min_length: int):
+    return " ".join([ w for w in text.split() if len(w) >= min_length])
+
+txt = "kerjf erk elr er elrgrkg"
+
+# version fonctionnelle
+# def sup_length(word, min_length=4):
+#     return len(word) >= min_length
+
+# fonction lambda, anaonyme, à usage unique
+# dont le code se résume à 1 expression
+# filter applique une fonction de filtre
+# (qui renvoie un booléen) sur un itérable (ex. une liste)
+# et qui renvoie les éléments de l'itérable qui
+# répondent vrai à la fonction
+def strip_lil_words(text, min_length=4):
+    return " ".join(filter(
+        lambda w: len(w) >= min_length, 
+        txt.split()
+    ))
+
+strip_lil_words(txt, 4)
 
 
 
@@ -12,3 +43,12 @@
 # (chaine de mots séparés par " ")
 # et en valeurs le nb d'occurence de ces mots dans le texte
 # ex. à bon chat bon rat => {"à": 1, "bon": 2, "chat: 1" ...}
+
+def get_occurences(text: str):
+    occ = {}
+    for w in text.split():
+        if w not in occ: occ[w] = 1
+        else: occ[w] += 1
+    return occ
+
+get_occurences("à bon chat bon rat")
