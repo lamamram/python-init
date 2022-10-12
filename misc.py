a181bdda5aba3a522d21b5fce5687a72c201b1cd
mlamamra <mlamamra@dawan.fr>
2022-10-12 09:11:33 +0200

    exo fonctions

---
+++ misc.py
@@ -0,0 +1,14 @@
+# %%
+# 1. écrire une fonction qui renvoie un texte donné
+# (chaine de mots séparés par " ")
+# qui ne conserve que les mots d'au moins 4 caractères
+
+
+
+
+# %%
+# 2. écrire une fonction qui renvoie un dictionnaires
+# présentant en clés les mots d'un texte donné
+# (chaine de mots séparés par " ")
+# et en valeurs le nb d'occurence de ces mots dans le texte
+# ex. à bon chat bon rat => {"à": 1, "bon": 2, "chat: 1" ...}
