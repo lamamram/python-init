9bbc4f912a647fe4d953c66e4441eaa594b427e8
mlamamra <mlamamra@dawan.fr>
2022-10-12 15:28:06 +0200

    cours regex

--- cours_regex.py
+++ cours_regex.py
@@ -10,3 +10,24 @@ import re
 
 # en langage Regex
 pattern = "[013456789][0-9]{4}|2[0-9AB][0-9]{3}"
+
+target_match = "44000 est mon cp"
+target_search = "mon cp est 44000 ou 2A320"
+
+# cherche à faire correspondre la regex depuis le début, pas à la fin
+# commence par la regex
+re.match(pattern, target_match)
+# print(re.match(pattern, target_search))
+
+# contient la regex => 1ère occurence
+re.search(pattern, target_search)
+
+# toutes les occurences
+list(re.finditer(pattern, target_search))
+re.findall(pattern, target_search)
+
+# chercher / remplacer
+re.sub(pattern, "*****", target_search)
+
+re.split(pattern, "2 rue du la rép 75013 PARIS")
+# %%
