3879c0c3a59a7b606527c0dede676a7785031278
mlamamra <mlamamra@dawan.fr>
2022-10-12 16:31:13 +0200

    cours poo: classe et objet instancié

--- cours_poo.py
+++ cours_poo.py
@@ -1,13 +1,19 @@
 # %%
 # CamelCase: les mots sont séparés par des majuscules
 class Account:
-    # attribut de classe
+    """
+    classe de gestion de compte en banque
+    """
+    # attribut de classe publique
     balance = 100.
 
-    # méthode
+    # méthode publique
     # le premier paramètre de toute méthode de classe (hors static)
     # représente une instance de la classe, et est nommé par convention self
     def deposit(self, amount: float):
+        """
+        dépôt: ajout d'une somme au compte
+        """
         # on accède aux attributs à travers self 
         self.balance += amount
 
@@ -17,4 +23,42 @@ acc.balance
 # du premier paramètre self de la def, car acc est self
 acc.deposit(200)
 acc.balance
+
+dir(Account)
+# %%
+class Account:
+    # préfixer un attribut (ou une méthode) par "__"
+    # le rend inaccessible depuis l'extérieur
+    __balance = 100.
+
+    # méthode privée: inaccessible depuis l'extérieur
+    def __set_balance(self, amount: float):
+        self.__balance += amount
+
+    # méthode publique: interface entre le contexte
+    # externe et les éléments privés
+    # sécurité, contrôle
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__set_balance(amount)
+    
+    def withdraw(self, amount: float):
+        if amount > 0:
+            self.__set_balance(-amount)
+    
+    def get_balance(self):
+        return self.__balance
+
+acc = Account()
+# l'accès aux méthodes et aux attributs est publique
+# par défaut
+# => utilisable par l'objet instancié, depuis l'extérieur de
+#la classe
+acc.deposit(200)
+# acc.__balance
+# encapsulation: on accède aux attributs privés depuis l'instance
+# qu'à travers les méthodes publiques
+acc.withdraw(100)
+acc.get_balance()
+# acc.__set_balance(200)
 # %%
