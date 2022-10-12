e3583e90193bbd16e05715c4865dd6d269fd58b9
mlamamra <mlamamra@dawan.fr>
2022-10-12 16:10:43 +0200

    cours poo: classe et objet instancié

--- cours_poo.py
+++ cours_poo.py
@@ -1,1 +1,20 @@
 # %%
+# CamelCase: les mots sont séparés par des majuscules
+class Account:
+    # attribut de classe
+    balance = 100.
+
+    # méthode
+    # le premier paramètre de toute méthode de classe (hors static)
+    # représente une instance de la classe, et est nommé par convention self
+    def deposit(self, amount: float):
+        # on accède aux attributs à travers self 
+        self.balance += amount
+
+acc = Account()
+acc.balance
+# quand on appelle une méthode, on ne tient pas compte
+# du premier paramètre self de la def, car acc est self
+acc.deposit(200)
+acc.balance
+# %%
