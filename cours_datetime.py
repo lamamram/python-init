d2f2f971dd8c867f04293c7fc3fb22a501d4363a
mlamamra <mlamamra@dawan.fr>
2022-10-12 13:44:11 +0200

    exo clock

--- cours_datetime.py
+++ cours_datetime.py
@@ -12,13 +12,45 @@ dt = datetime(*date_parts)
 dt
 
 # depuis une chaine de caractères
+# "p" comme parse
 date_str = "2022-10-12 11:52"
 dt = datetime.strptime(date_str, "%Y-%m-%d %H:%M")
 dt
 
 # %%
+# depuis un timestamp (nb de sceonds depuis le 1er
+# janvier 1970 à 00:00)
 dt = datetime.fromtimestamp(52 * 365.25 * 86400)
 dt
 
 # %%
-datetime.now()
+dt = datetime.now()
+# %%
+# exploitation des objets datetime
+dt.year, dt.month, dt.second # ...
+dt.date(), dt.time()
+# lundi: 0 => dimanche: 6
+dt.weekday()
+dt.timestamp()
+
+# formatage en chainede caractère
+# "f" comme format
+dt.strftime("%d/%m/%Y")
+dt.strftime("%A")
+
+# %%
+# arithmétique de dates
+
+new_year = datetime(2023, 1, 1)
+
+delta = new_year - datetime.now()
+delta.days
+
+# dates négatives ???
+delta = datetime.now() - new_year
+delta
+
+cuisson_oeuf_coque = timedelta(minutes=3)
+atable = datetime.now() + cuisson_oeuf_coque
+atable
+# %%
---
+++ exo_clock.py
@@ -0,0 +1,15 @@
+# %%
+# écrire une fonction time_remaining dans le module misc de utils
+# qui prend en paramètres les heures et minutes de la fin aujourd'hui (17:00)
+# et les heures et minutes de la pause cet arès moidi (15:45)
+# qui présente une boucle infinie (while True:)
+# dans laquelle on regarde toutes les minutes pour
+#   - afficher le temps restant en heure minutes secondes
+#   - afficher "c'est la pause" entre le début de la pause et + 15 min
+# à la fin, casser la boucle infinie
+# pour tester, préférer la commande python dans le terminal
+
+# 1. calculer le temps restant tout seul
+# 2. gérer le message si pause
+# 3. gérer l'interruption du programme
+# 4. gérer la boucle
