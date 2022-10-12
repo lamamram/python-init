e465e994fcf9a96cdf89b0fefef8c7fe457f4580
mlamamra <mlamamra@dawan.fr>
2022-10-12 15:15:05 +0200

    cours regex

---
+++ cours_regex.py
@@ -0,0 +1,12 @@
+# %%
+import re
+# une expression régulière (ou rationnelle) est une
+# chaine de caractère qui décrit un modèle de chaines de caractères
+# exemple de regex: un code postal français
+
+# 1er caractère 0,1,3,4,5,6,7,8 ou 9 et ensuite 4 chiffres
+# ou
+# 2 suivi de 0-9 ou A ou B suivi de 3 chiffres
+
+# en langage Regex
+pattern = "[013456789][0-9]{4}|2[0-9AB][0-9]{3}"
