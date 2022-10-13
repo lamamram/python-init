b013a9a943648ca2a3a43a8af5cddbd3719b734a
mlamamra <mlamamra@dawan.fr>
2022-10-13 15:57:13 +0200

    exo fichiers: récursion

--- exo_recursion.py
+++ exo_recursion.py
@@ -4,5 +4,12 @@
 # 2. on parcourt le dossier avec os.listdir
 # 3. pour chaque élément, si l'élément est un dossier
 # on boucle sur la fonction (attention à la valeur de retour)
-# 4. enregister les chemins de pycache dans une liste qu'in retourne
+# 4. enregister les chemins de pycache dans une liste qu'on retourne
 ## supprimer sequentiellement les chemins retournés
+
+def py_find(root, name, type="d") -> list:
+    found = []
+    # ...
+    return found
+
+py_caches = py_find(".", "__pycache__")
