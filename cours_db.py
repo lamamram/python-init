83d3a6dc1a2d37e66e2979499c89cc061b731f34
mlamamra <mlamamra@dawan.fr>
2023-03-07 17:11:36 +0100

    transformation d'une classe en gestionnaire de contexte

--- cours_db.py
+++ cours_db.py
@@ -31,16 +31,36 @@ class Sqlite3Client:
     def __init__(self, path, hydration="dict") -> None:
         self.__path = path
         self.__hydration = hydration
+    
+    # entrée dans le with
+    def __enter__(self):
+        self.__conn = sqlite3.connect(self.__path)
+        # retour du "as"
+        return self
+    
+    # sortie de with
+    # les params x_ décrivent une possible exception
+    def __exit__(self, x_typ, x_msg, x_trace):
+        # gestion de transactation
+        if x_typ:
+            self.__conn.rollback()
+        else:
+            self.__conn.commit()
+        self.__conn.close()
 
     def executescript(self, script_path, encoding="utf-8"):
-        with sqlite3.connect(self.__path) as conn:
-            if self.__hydration == "dict":
-                conn.row_factory = sqlite3.Row
-            cur = conn.cursor()
-            with open(script_path, "r", encoding=encoding) as sql_f:
-                cur.executescript(sql_f.read())
+        if self.__hydration == "dict":
+            self.__conn.row_factory = sqlite3.Row
+        cur = self.__conn.cursor()
+        with open(script_path, "r", encoding=encoding) as sql_f:
+            cur.executescript(sql_f.read())
 
 if __name__ == "__main__":
-    db = Sqlite3Client("./dns.db")
-    db.executescript("domain_names_sqlite3.sql")
+    # db = Sqlite3Client("./dns.db")
+    # db.executescript("domain_names_sqlite3.sql")
+    try:
+        with Sqlite3Client("./dns.db") as db:
+            db.executescript("domain_names_sqlite3.sql")
+    except sqlite3.OperationalError as e:
+        print(e)
 # %%
