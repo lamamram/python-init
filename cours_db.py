9494c03e94c9ff662922b2338385a9abe0755ace
mlamamra <mlamamra@dawan.fr>
2023-03-07 16:57:43 +0100

    passage à la poo

--- cours_db.py
+++ cours_db.py
@@ -24,3 +24,23 @@ with sqlite3.connect("./dns.db") as conn:
     cur.execute("SELECT COUNT(1) as nb FROM pays")
     row = dict(cur.fetchone())
     print(f"{row['nb']} lignes")
+# %%
+import sqlite3
+
+class Sqlite3Client:
+    def __init__(self, path, hydration="dict") -> None:
+        self.__path = path
+        self.__hydration = hydration
+
+    def executescript(self, script_path, encoding="utf-8"):
+        with sqlite3.connect(self.__path) as conn:
+            if self.__hydration == "dict":
+                conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            with open(script_path, "r", encoding=encoding) as sql_f:
+                cur.executescript(sql_f.read())
+
+if __name__ == "__main__":
+    db = Sqlite3Client("./dns.db")
+    db.executescript("domain_names_sqlite3.sql")
+# %%
