bcff80ea6047c20a448a94eadb315fa9b84ad095
mlamamra <mlamamra@dawan.fr>
2023-03-07 17:32:02 +0100

    requête préparées

--- cours_db.py
+++ cours_db.py
@@ -25,7 +25,7 @@ with sqlite3.connect("./dns.db") as conn:
     row = dict(cur.fetchone())
     print(f"{row['nb']} lignes")
 # %%
-import sqlite3
+import sqlite3, csv
 
 class Sqlite3Client:
     def __init__(self, path, hydration="dict") -> None:
@@ -54,13 +54,38 @@ class Sqlite3Client:
         cur = self.__conn.cursor()
         with open(script_path, "r", encoding=encoding) as sql_f:
             cur.executescript(sql_f.read())
-
+    
+    def insert(self, table_name: str, fields: tuple, values: list):
+        req = f"INSERT INTO {table_name} ({', '.join(fields)}) VALUES ({','.join((['?'] * len(fields)))})"
+        cur = self.__conn.cursor()
+        cur.executemany(req, raw_values)
+        return cur.rowcount
+        
 if __name__ == "__main__":
     # db = Sqlite3Client("./dns.db")
     # db.executescript("domain_names_sqlite3.sql")
+    with open("dns_100000.csv", "r", encoding="iso-8859-1") as f:
+        rd = csv.reader(f, delimiter=';')
+        next(rd)
+        raw_values = list(map(lambda r: r[:2] + [r[10]], list(rd)))
+        # print(raw_values[:5])
+    
     try:
         with Sqlite3Client("./dns.db") as db:
             db.executescript("domain_names_sqlite3.sql")
+            print(db.insert("domain_name", ("name", "iso2", "created_at"), raw_values))
     except sqlite3.OperationalError as e:
         print(e)
+# %%
+# créer une méthode publique insert
+# prends en paramètre 
+# - le nom de la table
+# - la liste des 3 champs de la table
+# - une liste de vecteurs de données
+# 3 champs (nom de domaine, pays, date de création)
+# pour construire la requête
+# "INSERT INTO domain_name (name, iso2, created_at) VALUES (?, ?, ?)"
+
+def insert(self, table_name: str, fields: tuple, values: list):
+    pass
 # %%
