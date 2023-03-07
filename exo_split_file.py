96ca7211062400fab16da28badfe7faa7b5bef9a
mlamamra <mlamamra@dawan.fr>
2023-03-07 15:26:22 +0100

    extraction csv

--- exo_split_file.py
+++ exo_split_file.py
@@ -1,5 +1,4 @@
 # %%
-import os
 """
 1. extraire le fichier .csv contenu dans le zip à télécharger
 hint: zipfile.Zipfile (doc ou google/stackoverflow)
@@ -18,3 +17,18 @@ hint: les zip s'ouvrent et se ferment
 
 modus operandi: faire ceci en n'ouvrant le csv en lecture qu'une seule fois
 """
+import os
+from zipfile import ZipFile
+
+ZIP_PATH = "./202105_OPENDATA_A-NomsDeDomaineEnPointFr.zip"
+DNS_PATH = "./dns.csv"
+
+if __name__ == "__main__":
+    if not os.path.exists(DNS_PATH):
+        with ZipFile(ZIP_PATH, "r") as zip_f:
+            zip_f.extractall()
+        filename = os.path.basename(ZIP_PATH)
+        base, ext = os.path.splitext(filename)
+        os.rename(base + ".csv", "dns.csv")
+
+# %%
