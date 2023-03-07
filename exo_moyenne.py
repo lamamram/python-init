2fd7ee403f4009807ee60110c35d100047c57145
mlamamra <mlamamra@dawan.fr>
2023-03-07 10:31:47 +0100

    fonctions lambda et gestion d'exception

--- exo_moyenne.py
+++ exo_moyenne.py
@@ -54,32 +54,59 @@ else:
     print(f"moyenne de {values}: {round(moy, 2)}")
 # %%
 # encapsulation en fonction
-import re
+import re, sys
+FLOAT_PATTERN = "(-?[0-9])+(?:\.[0-9]*)?"
 # *values: un ou n paramètres positionnels, disponibles
 # dans la foncition dans le tuple values
 # => les params positionnels officiels doivent être à gauche
 # les paramètres nommés à droite
 # **opts: arguments nommés supplémentaires qui sont dispo
 # dans un dictionnaire
+# def is_float(item: str) -> bool:
+#     return re.match(FLOAT_PATTERN, item)
+
 def calcul_moyenne(*values, float_pattern: float=FLOAT_PATTERN, precision: int=2, **opts):
     values = list(values)
     if not values: 
         print("liste vide")
         sys.exit(0)
 
-    for i, val in enumerate(values):
-        if re.match(float_pattern, val):
-            values[i] = float(val)
-        else:
-            if "debug"in opts and opts["debug"]:
-                print(f"{val} non convertible !!!")
-            break
-    else:
-        moy = sum(values) / len(values)
-        return round(moy, precision)
+    # 1. filtrer sur ce qui est convertible
+    # filtered = list(filter(is_float, values))
+    filtered = list(filter(
+        lambda val: re.match(float_pattern, val), 
+        values
+    ))
+    issues = list(set(values) - set(filtered))
+    if issues:
+        if "debug" in opts and opts["debug"]:
+            print(f"{issues}: non convertibles !!!")
+        # lever une exception
+        raise ValueError(f"{issues}: non convertibles !!!")
+    # 2. appliquer la conversion sur les vecteur de données
+    converted = list(map(float, filtered))
+    moy = sum(converted) / len(converted)
+    return round(moy, precision)
 
-calcul_moyenne("1.3", "truc", "3.14", precision=3)
-calcul_moyenne(*["1.3", "truc", "3.14"], precision=3, debug=True)
+
+# calcul_moyenne("1.3", "truc", "3.14", precision=3)
+try:
+#    calcul_moyenne(*["1.3", "truc", "3.14"], precision=3, debug=True)
+   m = calcul_moyenne(*["1.3", "3.14"], precision=3)
+   3/0
+except (ValueError, TypeError) as e:
+    print(e)
+# except Exception as e:
+#     print("erreur non anticipée")
+# s'exécute en cas de try sans exception levée,
+# sans crash
+else:
+    print(f"résultat: {m}")
+# s'exécute même après exit, 
+# même après un crash
+finally:
+    print("before leaving !!!")
+
 # %%
 # %%
 # effet des arguments * à l'appel
@@ -89,4 +116,11 @@ def func(p1, p2):
 params = ["p1", "p2"]
 func(params[0], params[1])
 func(*params)
+# %%
+# * force les paramètres à droite à être appelés
+# de manière nommée
+def connect(*, host, username, passwd, dbname, port=3306, encoding="utf-8"):
+    print("connect")
+
+connect(username="root", host="127.0.0.1", passwd="123456", dbname="project")
 # %%
