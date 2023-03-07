f9b465fb5a5b5d82d0e1d00476c4d057655f08ea
mlamamra <mlamamra@dawan.fr>
2023-03-07 16:32:58 +0100

    hydration en dictionnaire

--- cours_db.py
+++ cours_db.py
@@ -4,11 +4,14 @@ import sqlite3
 
 # 1. cnx
 with sqlite3.connect("./dns.db") as conn:
+    # classe de réponse de type dict
+    conn.row_factory = sqlite3.Row
     # acquisition d'un prompt
     cur = conn.cursor()
     # exécution
     cur.execute("SELECT SQLITE_VERSION() as version")
     # récupération
     row = cur.fetchone()
-    print(row)
+    print(dict(row))
 # closed
+# %%
