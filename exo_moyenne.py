c46d57052c06e9fbb21af0eab89d4f3e703a7765
mlamamra <mlamamra@dawan.fr>
2024-07-08 16:14:02 +0200

    exo moyenne consigne

---
+++ exo_moyenne.py
@@ -0,0 +1,11 @@
+"""
+1/ saisir n de valeurs entiers relatifs séparés par ","
+2/ on veut itérer sur les valeurs pour vérifier
+que ces valeur sont des entiers relatifs
+3/ si c'est convertible on ajoute la valeur convertie dans une liste
+3b/ si ce n'est pas convertible => "casser la boucle"
+4/ calculer la moyenne depuis la liste
+4/ présenter le résultat avec 2 chiffres sign.  
+"""
+
+# %%
