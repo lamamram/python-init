7d612edcfff3b75fc06c099307bb956e365abcd0
mlamamra <mlamamra@dawan.fr>
2024-07-09 10:19:31 +0200

    exemples str, list, tuples, dict

--- 080724/cours_builtins_advanced.py
+++ 080724/cours_builtins_advanced.py
@@ -33,3 +33,98 @@ while sentence[start:].count("chat"):
     start = ind + len("chat")
     ## IL FAUT QUE LE BLOC CHANGE LE TEST !!!! SINON INFINITE LOOP
 # %%
+
+lst, tpl, word = [-1, 2, 3], (-1, 2, 3), "bonjour"
+# list est MUTABLE
+lst[0] = 1 # OK, modification partielle, id(lst) inchangé
+# str et tuple sont IMMUTABLE
+# word[0] = "B" # TypeError
+# tpl[0] = -1 # TypeError
+word, tpl = "Bonjour", (1, 2, 3) # OK mais modifie id(word) et id(tpl)
+# %%
+# modification de str
+word = "bonjour"
+
+# retourne la transformation
+# mais word inchangé
+print(word.capitalize())
+print(word)
+word = word.capitalize()
+
+# %% 
+# modification de listes
+
+lst = [1, 2, 3, 4]
+dir(lst)
+# pop retourne et supprime
+while lst:
+    print(lst.pop())
+
+legit_animals = ["chat", "chien", "ane", "cheval", "chat"]
+while legit_animals.count("chat"):
+    legit_animals.remove("chat")
+legit_animals
+
+# %%
+
+# attention au unpacking
+
+a, b = 0, 1
+
+a, b = b, a + b # ici le a de droite vaut 0
+
+a = b
+b = a + b # ici le a de droite vaut 1
+
+a, b
+# %%
+# création de clés / paires de dico VS création d'éléments de list
+
+dico, lst = {"k": "v"}, [1, 2, 3]
+
+dico["k2"] = "v2"
+# lst[3] = 4 # IndexError
+
+lst.append(4)
+# %%
+# checker si une clé existe dans un dico pour utiliser sa valeur
+
+dico = {"k": "v"}
+
+# if "truc" in dico:
+#     print(dico["truc"])
+# else:
+#     print("default")
+
+dico.get("truc", "default")
+dico.get("k", "default")
+# %%
+
+x = 1
+del x
+# x + 1 # NameError 
+# %%
+
+lst = [-1,3]
+del lst[0]
+lst
+# %%
+# objets intermédiaires
+dico = {"k": "v"}
+dico.values() # objet "dict_values"
+list(dico.values())
+
+dico.items() # objet "dict_items"
+list(dico.items())
+# %%
+# utilisation des .values et .items
+
+dico = {"k": "v", "k2": "v2", "k3": "v3"}
+
+for k in dico: print(k)
+
+for v in dico.values(): print(v)
+
+# unpacking sous jacent depuis les tuples gérés dans l'objet dict_items
+for k, v in dico.items(): print(k, v)
+# %%
